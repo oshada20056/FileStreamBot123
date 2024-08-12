26baff49ee6664a60ae8c17e8bb76bbf7c2bfb58
oshada20056 <153474645+oshada20056@users.noreply.github.com>
2024-08-13 02:55:05 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,13 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 1234))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 1234567890))
+    API_ID = int(env.get("TELEGRAM_API_ID", 21204722))
+    API_HASH = env.get("TELEGRAM_API_HASH", "4f5b4bbc15e7f9df9961ac92e8fd219b")
+    OWNER_ID = int(env.get("OWNER_ID", 5310455183))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "BotFather")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234:abcd")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001234567890))
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "Film_Studio12_bot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "6214642855:AAEC_rCt7ri2VxlvEdsP0CtT4KnhQYdvDVY")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001941874825))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
